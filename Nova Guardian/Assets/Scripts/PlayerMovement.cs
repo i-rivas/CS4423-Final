d6a6fc59c0ca7fd70a70985dc82784da921ac68a
i-rivas <iriva002@outlook.com>
2023-11-11 12:27:13 -0600

    Made various Changes

--- Nova Guardian/Assets/Scripts/PlayerMovement.cs
+++ Nova Guardian/Assets/Scripts/PlayerMovement.cs
@@ -5,26 +5,52 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    
+    private SpriteRenderer sprite;
+    private Animator anim;
 
+    private float dirX = 0f;
+   [SerializeField] private float moveSpeed = 7f;
+   [SerializeField] private float jumpForce = 14f;
     
     
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxisRaw("Horizontal");
 
-        rb.velocity = new Vector2(dirX * 7f, rb.velocity.y);
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 14f);
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimationState();
+    }
+
+    private void UpdateAnimationState()
+    {
+        if(dirX > 0f)
+        {
+            anim.SetBool("walking", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0f)
+        {
+            anim.SetBool("walking", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("walking", false);
         }
     }
 }
